8a768ab9895640813b7914b8afe55c7b04dd4f1d
LuisEnrique <fernandez.luis@pucp.pe>
2022-05-08 17:33:36 -0500

    vector ordenado

--- ord_burbuja.c
+++ ord_burbuja.c
@@ -6,12 +6,26 @@
 
 int main()
 {
+    int aux;
+    int i, j;
     int ord_vector[SIZE];
     srand(time(NULL));
     printf("Generacion de numeros aleatorios");
     for (int i = 0; i < SIZE; i++)
     {
-        ord_vector[i] = 100 + rand()%(101);
+        ord_vector[i] = 100 + rand() % (101);
         printf("\n%d", ord_vector[i]);
+    }
+    for (i = 0; i < SIZE; i++)
+    {
+        for (j = 0; j < SIZE; j++)
+        {
+            if (ord_vector[j] > ord_vector[j + 1])
+            {
+                aux = ord_vector[j];
+                ord_vector[j] = ord_vector[j + 1];
+                ord_vector[j + 1] = aux;
+            }
+        }
     }
 }
